652a5a0c60e7e63a1e4dd5ca8c4a6def052c72fd
Luis Armando <mariana@MacBook-Air-de-Luis-Armando.local>
2024-12-17 19:35:15 -0800

    setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Modal from "../components/Modal/Modal.svelte";
+  import GlobalData from "../components/GlobalData/GlobalData.svelte";
+  import Notification from "../components/Notification/Notification.svelte";
+  import ShaderToy from "../components/ShaderToy/ShaderToy.svelte";
+  import OShader from "../components/ShaderToy/O.shader";
+    import Logic from "../components/Logic/Logic.svelte";
+</script>
+
+<GlobalData />
+
+<style lang="scss">
+  @import "./styles/reset.scss";
+  @import "./styles/global.scss";
+</style>
